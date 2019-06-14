d9fd702ecd104369fc5584927be0f815edc4f371
Nijat Hamid <nicatorium@gmail.com>
2019-06-14 10:23:35 +0100

    Add missing SceneDelegate

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,20 +1,36 @@
-//
-//  ViewController.swift
-//  Dicee-iOS13
-//
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slotOne: UIImageView!
+    @IBOutlet weak var slotTwo: UIImageView!
+
+    @IBAction func generate(_ sender: UIButton) {
+        slotOne.image = UIImage(named: randomDiceImage())
+        slotTwo.image = UIImage(named: randomDiceImage())
+    }
+    
+    @IBAction func reset(_ sender: UIButton) {
+        slotOne.image = nil
+        slotTwo.image = nil
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let imageViews = [slotOne, slotTwo]
+        for imageView in imageViews {
+            setupImageView(for: imageView!, radius: 12)
+        }
+    }
+    
+    private func randomDiceImage() -> String {
+        return "Dice\(Int.random(in: 1...6))"
     }
 
+    private func setupImageView(for imageView: UIImageView, radius: CGFloat) {
+        imageView.layer.cornerRadius = radius
+        imageView.clipsToBounds = true
 
+    }
 }
-
